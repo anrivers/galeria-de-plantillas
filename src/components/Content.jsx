5ea8e86f2eca052589b2a055a2da44b43a50c710
anrivers <anrivers26@gmail.com>
2024-05-28 19:03:03 -0600

    Creación del maquetado del contenido principal con su componente y su estilo

---
+++ src/components/Content.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import './Content.css'; 
+
+const Content = () => {
+  return (
+    <main className="content">
+        <section id="home">
+          <h2>PLANTILLAS WEB</h2>
+          <p>Elige el diseño para crear tu sitio. Puedes cambiar el color, texto y todo a tu gusto.
+          </p>
+        </section>
+        <section id="about">
+          <h2>Acerca de</h2>
+          <p>Información sobre nosotros.</p>
+        </section>
+        <section id="contact">
+          <h2>Contacto</h2>
+          <p>Cómo contactarnos.</p>
+        </section>
+        <section id="contact">
+          <h2>Ejemplo de contenido</h2>
+          <p>CLorem ipsum dolor sit amet, consectetur adipisicing elit, sed dousmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor..</p>
+        </section>
+        <section id="contact">
+          <h2>Ejemplo de contenido</h2>
+          <p>CLorem ipsum dolor sit amet, consectetur adipisicing elit, sed dousmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor..</p>
+        </section>
+        <section id="contact">
+          <h2>Ejemplo de contenido</h2>
+          <p>CLorem ipsum dolor sit amet, consectetur adipisicing elit, sed dousmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor..</p>
+        </section>
+        <section id="contact">
+          <h2>Ejemplo de contenido</h2>
+          <p>CLorem ipsum dolor sit amet, consectetur adipisicing elit, sed dousmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor..</p>
+        </section>
+        <section id="contact">
+          <h2>Ejemplo de contenido</h2>
+          <p>CLorem ipsum dolor sit amet, consectetur adipisicing elit, sed dousmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor..</p>
+        </section>
+      </main>
+  );
+}
+
+export default Content;
