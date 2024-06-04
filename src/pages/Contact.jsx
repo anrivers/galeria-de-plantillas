2432ff6218fe32f7bfc9480802f7da3c4106b7b9
anrivers <anrivers26@gmail.com>
2024-06-04 17:59:26 -0600

    Creación de archivos de contacto y sobre nosotros, primeros archivos de prueba

---
+++ src/pages/Contact.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from '../components/Header'
+import FooterComponent from '../components/Footer'
+
+export const Contact = () => {
+  return (
+    <div>
+    <Header></Header>
+        <h1>Hola esto es el apartado de contacto</h1>
+    <FooterComponent></FooterComponent>
+        </div>
+  )
+}
