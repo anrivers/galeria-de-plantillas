9f71bc358e5d88734ce19f39f1001ce741bbf685
anrivers <anrivers26@gmail.com>
2024-06-04 17:52:34 -0600

    Se agregó el link de router-dom al archivo de Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import './Header.css';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   const [menuActive, setMenuActive] = useState(false);
@@ -15,9 +16,9 @@ const Header = () => {
       </div>
       <nav className={`nav-links ${menuActive ? 'active' : ''}`}>
         <ul>
-          <li><a href="#home">Inicio</a></li>
-          <li><a href="#about">Sobre Nosotros</a></li>
-          <li><a href="#contact">Contacto</a></li>
+          <li><Link to="/">Inicio</Link></li>
+          <li><Link to="/Aboutus">Sobre Nosotros</Link></li>
+          <li><Link to="/Contact">Contacto</Link></li>
         </ul>
       </nav>
       <button className="menu-toggle" onClick={toggleMenu}>
