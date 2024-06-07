4f2cb085dd7ee4ba6e826a171e87dee6db3ad4f8
anrivers <anrivers26@gmail.com>
2024-06-07 15:55:14 -0600

    Creación de archivos js en la carpeta de pages, para llamar los componentes de 'contacto' y 'sobre nosotros'

---
+++ src/pages/AboutUsPage.js
@@ -0,0 +1,17 @@
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+import { AboutUs } from '../components/Aboutus';
+import React from 'react';
+
+
+function AboutUsPage() {
+  return (
+<div> 
+    <Header></Header>
+    <AboutUs></AboutUs>
+    <Footer></Footer>
+</div>
+  );
+}
+
+export default AboutUsPage;
---
+++ src/pages/ContactPage.js
@@ -0,0 +1,17 @@
+import Header from '../components/Header';
+import { Contact } from '../components/Contact';
+import Footer from '../components/Footer';
+import React from 'react';
+
+
+function ContactPage() {
+  return (
+<div> 
+    <Header></Header>
+    <Contact></Contact>
+    <Footer></Footer>
+</div>
+  );
+}
+
+export default ContactPage;
