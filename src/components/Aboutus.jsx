2307122ab934327f9b0c5624c19b5079e584cd49
anrivers <anrivers26@gmail.com>
2024-06-07 15:54:14 -0600

    Creación del archivo Aboutus. jsx para el componente de 'Sobre nosotros', y su CSS para el estilo de la página de 'sobre nosotros'

---
+++ src/components/Aboutus.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import './Aboutus.css';
+
+export const AboutUs = () => {
+    return (
+        <div>
+            <div className="about-container">
+      <h2>SOBRE NOSOTROS</h2>
+      <p>Somos una empresa dedicada a ofrecer las mejores plantillas web para tu sitio. Nuestro equipo está compuesto por diseñadores y desarrolladores con amplia experiencia en el campo.</p>
+      <div className="about-content">
+        <div className="about-section">
+          <img src="images/estructura-web.png" alt="Nuestra Misión" className="about-image"/>
+          <div className="about-text">
+            <h3>Nuestra Misión</h3>
+            <p>Impulsar el éxito empresarial proporcionando soluciones digitales personalizadas, optimizando operaciones y mejorando la experiencia del usuario.</p>
+          </div>
+        </div>
+        <div className="about-section reverse">
+          <img src="images/estructura-web.png" alt="Nuestro Equipo" className="about-image"/>
+          <div className="about-text">
+            <h3>Nuestro Equipo</h3>
+            <p>Contamos con un equipo multidisciplinario de profesionales apasionados por el diseño web y la tecnología.</p>
+          </div>
+        </div>
+        <div className="about-section">
+          <img src="images/estructura-web.png" alt="Nuestros Valores" className="about-image"/>
+          <div className="about-text">
+            <h3>Nuestros Valores</h3>
+            <ul>
+              <li>Innovación</li>
+              <li>Responsabilidad</li>
+              <li>Transparencia</li>
+              <li>Flexibilidad</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+        </div>
+    );
+}
