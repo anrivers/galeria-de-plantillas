2bc99665db023657bf7b8ab234b53938fcea8eef
anrivers <anrivers26@gmail.com>
2024-06-07 18:18:46 -0600

    Actualización de nueva ruta

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import ContactPage from './pages/ContactPage';
 import Main from './pages/Main';
 import AboutUsPage from './pages/AboutUsPage';
+import CardsDetailPage from './pages/CardsDetailPage';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<Main/>}/>
         <Route path="/Contact" element={<ContactPage/>}/>
         <Route path="/AboutUs" element={<AboutUsPage/>}/>
+        <Route path="/CardsDetail" element={<CardsDetailPage/>}/>
         </Routes>
       </Router>
   </div>
