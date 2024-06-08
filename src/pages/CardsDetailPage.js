0781e80e8452d000aa0a4363eec9b3fbc05f680e
anrivers <anrivers26@gmail.com>
2024-06-07 18:23:04 -0600

    Creación de archivo js en la carpeta de pages, para llamar los componentes de 'cardsdetail'

---
+++ src/pages/CardsDetailPage.js
@@ -0,0 +1,17 @@
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+import React from 'react';
+import CardsDetail from '../components/CardsDetail';
+
+
+function CardsDetailPage() {
+  return (
+<div> 
+    <Header></Header>
+    <CardsDetail></CardsDetail>
+    <Footer></Footer>
+</div>
+  );
+}
+
+export default CardsDetailPage;
