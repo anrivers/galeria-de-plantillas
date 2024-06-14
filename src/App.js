ca68165d4c5933e4a80679600337b1a72242c450
anrivers <anrivers26@gmail.com>
2024-06-14 16:56:06 -0600

    Se agregaron nuevas rutas

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import ContactPage from './pages/ContactPage';
 import Main from './pages/Main';
 import AboutUsPage from './pages/AboutUsPage';
 import CardsDetailPage from './pages/CardsDetailPage';
+import EditPage from './pages/EditPage';
+import ViewPage from './pages/ViewPage';
+import DowloadPage from './pages/DowloadPage';
 
 function App() {
   return (
@@ -13,7 +16,12 @@ function App() {
         <Route path="/" element={<Main/>}/>
         <Route path="/Contact" element={<ContactPage/>}/>
         <Route path="/AboutUs" element={<AboutUsPage/>}/>
-        <Route path="/CardsDetail" element={<CardsDetailPage/>}/>
+        <Route path="/CardsDetail/:id" element={<CardsDetailPage/>} />
+        <Route path="/ViewTemplate/:id" element={<ViewPage/>} />
+        <Route path="/EditTemplate/:id" element={<EditPage/>} />
+        <Route path="/DownloadTemplate/:id" element={<DowloadPage/>} />
+        
+
         </Routes>
       </Router>
   </div>
