01fde2b99c6ecb55a278051aa7b6f0791212bae5
anrivers <anrivers26@gmail.com>
2024-06-20 16:27:10 -0600

    Se agregaron imagenes para la página sobre nosotros

--- src/components/Aboutus.jsx
+++ src/components/Aboutus.jsx
@@ -9,21 +9,21 @@ export const AboutUs = () => {
       <p>Somos una empresa dedicada a ofrecer las mejores plantillas web para tu sitio. Nuestro equipo está compuesto por diseñadores y desarrolladores con amplia experiencia en el campo.</p>
       <div className="about-content">
         <div className="about-section">
-          <img src="images/estructura-web.png" alt="Nuestra Misión" className="about-image"/>
+          <img src="images/aboutus1.jpg" alt="Nuestra Misión" className="about-image"/>
           <div className="about-text">
             <h3>Nuestra Misión</h3>
             <p>Impulsar el éxito empresarial proporcionando soluciones digitales personalizadas, optimizando operaciones y mejorando la experiencia del usuario.</p>
           </div>
         </div>
         <div className="about-section reverse">
-          <img src="images/estructura-web.png" alt="Nuestro Equipo" className="about-image"/>
+          <img src="images/aboutus2.jpg" alt="Nuestro Equipo" className="about-image"/>
           <div className="about-text">
             <h3>Nuestro Equipo</h3>
             <p>Contamos con un equipo multidisciplinario de profesionales apasionados por el diseño web y la tecnología.</p>
           </div>
         </div>
         <div className="about-section">
-          <img src="images/estructura-web.png" alt="Nuestros Valores" className="about-image"/>
+          <img src="images/aboutus3.jpg" alt="Nuestros Valores" className="about-image"/>
           <div className="about-text">
             <h3>Nuestros Valores</h3>
             <ul>
