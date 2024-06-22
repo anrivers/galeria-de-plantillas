30c136bc55cc7a951be19c3464d995f8e810e976
anrivers <anrivers26@gmail.com>
2024-06-21 18:55:51 -0600

    Se agregó taiwlind css

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+import './index.css';
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import { DndProvider } from 'react-dnd'
 import { HTML5Backend } from 'react-dnd-html5-backend';
