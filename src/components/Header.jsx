4e9594f9fd0d8f401a1532a4dcca2464539ecbf0
anrivers <anrivers26@gmail.com>
2024-06-21 19:03:42 -0600

    Se modificó componente header agregando tailwindcss

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import './Header.css';
 import { Link } from 'react-router-dom';
 
 const Header = () => {
@@ -10,19 +9,21 @@ const Header = () => {
   };
 
   return (
-    <header className="header">
-      <div className="logo">
+    <header className="bg-gray-200 text-black py-5 px-5 lg:px-10 flex justify-between items-center flex-wrap">
+      <div className="logo text-lg">
         <h1>GALERÍA DE PLANTILLAS</h1>
       </div>
-      <nav className={`nav-links ${menuActive ? 'active' : ''}`}>
-        <ul>
-          <li><Link to="/">Inicio</Link></li>
-          <li><Link to="/Aboutus">Sobre Nosotros</Link></li>
-          <li><Link to="/Contact">Contacto</Link></li>
+      <nav className={`nav-links ${menuActive ? 'block' : 'hidden'} mt-4 lg:mt-0 lg:flex lg:items-center w-full lg:w-auto lg:justify-center`}>
+        <ul className="flex flex-col items-center lg:flex-row gap-4 lg:gap-8 lg:text-lg">
+          <li><Link to="/" className="text-black hover:text-white hover:bg-blue-700 px-3 py-1 rounded transition duration-300 block">Inicio</Link></li>
+          <li><Link to="/Aboutus" className="text-black hover:text-white hover:bg-blue-700 px-3 py-1 rounded transition duration-300 block">Sobre Nosotros</Link></li>
+          <li><Link to="/Contact" className="text-black hover:text-white hover:bg-blue-700 px-3 py-1 rounded transition duration-300 block">Contacto</Link></li>
         </ul>
       </nav>
-      <button className="menu-toggle" onClick={toggleMenu}>
-        &#9776;
+      <button className="menu-toggle lg:hidden flex justify-center items-center" onClick={toggleMenu}>
+        <div className="block">
+          &#9776;
+        </div>
       </button>
     </header>
   );
