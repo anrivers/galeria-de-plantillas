c85b38446a82d158788257bcbaaa37e68556a200
anrivers <anrivers26@gmail.com>
2024-07-04 17:23:34 -0600

    Se modificó el banner, agregando estilo con base en tailwind css

---
+++ src/components/Banner.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import '../index.css'; 
+
+export const Banner = () => {
+    return (
+<section
+  className="relative bg-[url(https://images.unsplash.com/photo-1569012871812-f38ee64cd54c?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)] bg-cover bg-center bg-no-repeat"
+>
+  <div
+     className="absolute inset-0 bg-white/75 sm:bg-transparent sm:from-white/95 sm:to-white/25 ltr:sm:bg-gradient-to-r rtl:sm:bg-gradient-to-l"
+  ></div>
+
+  <div
+    className="relative mx-auto max-w-screen-xl px-4 py-32 sm:px-6 lg:flex lg:h-screen lg:items-center lg:px-8"
+  >
+    <div className="max-w-xl text-center ltr:sm:text-left rtl:sm:text-right">
+      <h1 className="text-3xl font-extrabold text-black sm:text-5xl">
+        PLANTILLAS WEB
+
+        <strong className="block font-extrabold text-blue-700"> PREDISEÑADAS. </strong>
+      </h1>
+
+      <p className="mt-4 max-w-lg text-black sm:text-xl/relaxed">
+      ¡Comienza hoy mismo y crea un sitio web que te destaque en línea!
+      </p>
+    </div>
+  </div>
+</section>
+    );
+};
---
+++ src/pages/FAQPage.js
@@ -0,0 +1,17 @@
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+import { FAQ } from '../components/FAQ';
+import React from 'react';
+
+
+function FAQPage() {
+  return (
+<div> 
+    <Header></Header>
+    <FAQ></FAQ>
+    <Footer></Footer>
+</div>
+  );
+}
+
+export default FAQPage;
