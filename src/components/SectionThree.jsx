9ce88bdda1ff1428cb79c52c62c44f8f554013e5
anrivers <anrivers26@gmail.com>
2024-07-08 09:58:51 -0600

    Se centró la section para que en todos los dispositivos se vea correctamente

--- src/components/SectionThree.jsx
+++ src/components/SectionThree.jsx
@@ -5,9 +5,9 @@ import { Link } from "react-router-dom";
 export const SectionThree = () => {
     return (
 <section>
-  <div className="max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8 lg:py-16">
+<div className="max-w-screen-xl mx-auto px-4 py-8 sm:px-6 sm:py-12 lg:px-8 lg:py-16">
     <div className="grid grid-cols-1 gap-y-8 lg:grid-cols-2 lg:items-center lg:gap-x-32">
-      <div className="mx-auto max-w-lg text-center lg:mx-0 ltr:lg:text-left rtl:lg:text-right">
+        <div className="mx-auto max-w-lg text-center lg:text-center">
         <h2 className="text-3xl font-bold sm:text-4xl">Ventajas de nuestras plantillas</h2>
 
         <p className="mt-4 text-gray-600">
